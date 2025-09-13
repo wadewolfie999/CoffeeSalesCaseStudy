021b500ac31e2c24cdd303323ae31a5e77e14819
Vahid <vahid@Vahids-MacBook-Air.local>
2025-09-13 15:52:32 +0330

    Added table creation and insert queries

---
+++ sql/set-cafedb.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE CoffeeSalesDB;
+GO
+
+USE CoffeeSalesDB;
+go
+
+CREATE TABLE Sales (
+transaction_id INT PRIMARY KEY,
+transaction_date DATE,
+transaction_time TIME,
+transaction_qty INT,
+store_id INT,
+store_location VARCHAR(100),
+product_id INT,
+unit_price FLOAT,
+product_category VARCHAR(100),
+product_type VARCHAR(100),
+product_detail VARCHAR(100)
+);
+go
+
+
